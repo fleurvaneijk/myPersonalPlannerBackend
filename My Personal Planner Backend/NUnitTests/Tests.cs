8da5e08d3c01bfa4047a8a4398da93602ccacb02
Fleur <fleur.vaneijk99@gmail.com>
2020-04-16 12:43:57 +0200

    nunit test trial 2

---
+++ My Personal Planner Backend/NUnitTests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace My_Personal_Planner_Backend.NUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true, "failed");
+        }
+    }
+}
