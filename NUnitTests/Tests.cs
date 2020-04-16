118a158578052d1fbf2e86431ca78dbbaa8fa933
Fleur <fleur.vaneijk99@gmail.com>
2020-04-16 11:45:54 +0200

    nunit test

---
+++ NUnitTests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(1 == 1, "1 is not 1, but how?");
+        }
+    }
+}
