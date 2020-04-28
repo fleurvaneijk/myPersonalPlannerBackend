e6b76a54c82a5a5f5764252db27968da2149e24a
Stefan de Keijzer <stefandekeijzer@xs4all.nl>
2020-04-28 15:41:13 +0200

    Changed Project

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace My_Personal_Planner_Backend.Controllers
+namespace MyPersonalPlannerBackend.Controllers
 {
     [ApiController]
     [Route("[controller]")]
