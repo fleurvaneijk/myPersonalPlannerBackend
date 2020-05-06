2da8b1818d62b6b2508ea2752c57ef719caab0f8
Stefan de Keijzer <stefandekeijzer@xs4all.nl>
2020-05-06 16:45:56 +0200

    Added a connection to the database

---
+++ Model/MariaDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyPersonalPlannerBackend.Model
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseMySql("Server=localhost; Database=mypersonalplanner;User=root;Password=password");
+    }
+}
