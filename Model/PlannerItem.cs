37184c7aca319028556b15e03fdbe54c9ef705e2
Stefan de Keijzer <stefandekeijzer@xs4all.nl>
2020-06-10 13:56:47 +0200

    Fixed code smells

--- Model/PlannerItem.cs
+++ Model/PlannerItem.cs
@@ -8,7 +8,7 @@ namespace MyPersonalPlannerBackend.Model
         public Planner? Planner { get; set; }
         public int User { get; set; }
         public int Day { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public string? Description { get; set; }
         public bool? IsDone { get; set; }
 
